8a31ae9fa7dc6fb13d40b28d038835d93d942b50
Manchem_Viswanadham <viswanadham.mancheam@gmail.com>
2026-02-28 19:53:17 +0530

    Terraform Practice

---
+++ EC2/EC2.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "Allow_All" {
+  name        = "Allow_All"
+  description = "Allow inbound traffic and all outbound traffic"
+    ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1" #Allows all traffic
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+    egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1" #ALL traffic
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+    tags = {
+        Name = "Allow_All"
+  }
+} 
